4c75bc0a37728cd2a8644772314f70ce39c27241
austin-9903 <austin.a@zohocorp.com>
2025-12-14 00:35:43 +0530

    Page initial content and configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,69 @@
 import Head from "next/head";
 import Header from "@/components/Header";
+import { sanityClient } from "../../sanity";
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="min-h-screen mx-auto max-w-7xl">
       <Head>
         <title>Medium</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
       <Header />
+
+      <div className="flex items-center justify-center border-y border-black bg-yellow-300 py-10 lg:py-0">
+        <div className="space-y-5 px-10">
+          <h1 className="max-w-xl font-serif text-6xl">
+            <span className="underline decoration-black decoration-4">Medium</span> {' '} is a place to write, read, and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect with millions of readers.
+          </h2>
+        </div>
+        <img 
+          className="hidden h-32 md:inline-flex lg:h-full lg:w-80 ml-10"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" 
+          alt="Medium Logo" 
+        />
+      </div>
+
+      <div className="p-10">
+        <h1 className="text-3xl">Latest Posts
+          </h1>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  try {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    slug,
+    author->{
+      name,
+      image
+    },
+    mainImage
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return {
+      props: {
+        posts: [],
+      },
+    };
+  }
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { FC } from 'react'
+import Link from 'next/link'
 
-const Header = () => {
+const Header:FC = () => {
   return (
-    <div>Hi Austin. This is header</div>
+    <header className="mx-auto flex max-w-7xl justify-between items-center p-5">
+        <div className="flex items-center space-x-5">
+            <Link href="/">
+                <img className="w-44 object-contain cursor-pointer" src="https://miro.medium.com/v2/resize:fit:8978/1*s986xIGqhfsN8U--09_AdA.png" alt="Medium Logo" />
+            </Link>
+            <h3 className="hidden md:inline-block">About</h3>
+            <h3 className="hidden md:inline-block">Contact</h3>
+            <button className="rounded-full bg-green-400 px-4 py-2 text-white">Follow</button>
+        </div>
+
+        <div className="flex items-center space-x-5 text-sm">
+            <h3 className="hidden md:inline-block text-green-400">Sign In</h3>
+            <button className="rounded-full text-green-400 px-4 py-2 border border-green-400">Get Started</button>
+        </div>
+    </header>
   )
 }
 
